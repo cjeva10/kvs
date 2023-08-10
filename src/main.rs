121dd01d256b6f827ba2d5298d9441f782873201
cjeva10 <cjeavnko@gmail.com>
2023-08-10 14:44:51 -0400

    main uses from_bytes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use resp::{Resp, Result};
 
 fn main() -> Result<()> {
-    let s = "*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n";
+    let s = b"*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n";
+    let resp = Resp::from_bytes(s)?;
 
-    let resp = Resp::from_str(s)?;
-
-    assert_eq!(s.to_string(), resp.to_string());
+    assert_eq!(s, resp.to_string().as_bytes());
 
     Ok(())
 }
