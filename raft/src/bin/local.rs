f5d16ff9364c7ef19b273656bfda82d494238f77
cjeva10 <cjeavnko@gmail.com>
2023-08-24 16:12:32 -0400

    added executable for local raft cluster

--- raft/src/bin/local.rs
+++ raft/src/bin/local.rs
@@ -1,18 +1,20 @@
 use eyre::Result;
-use raft::init_nodes;
+use raft::init_local_nodes;
+
+const MIN_DELAY: u64 = 100;
+const NUM_NODES: usize = 5;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let num: usize = 3;
-
-    let (nodes, _) = init_nodes(num);
+    let (nodes, _) = init_local_nodes(NUM_NODES);
 
     let mut handles = Vec::new();
+
     for node in nodes {
         handles.push(tokio::spawn( async {
-            node.start().unwrap();
+            node.start(MIN_DELAY).unwrap();
         }));
     }
 
